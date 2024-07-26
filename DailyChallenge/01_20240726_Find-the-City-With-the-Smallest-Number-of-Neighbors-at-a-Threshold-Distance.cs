44e8132434ffc9f4751a1a8a3b14d4b29689907c
Jose Villa <Jose.Villa@version1.com>
2024-07-26 13:09:49 +0200

    Daily challenge 20240726

---
+++ DailyChallenge/01_20240726_Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.cs
@@ -0,0 +1,74 @@
+﻿namespace DailyChallenge
+{
+	public class Solution01
+	{
+#pragma warning disable S2368 // Public methods should not have multidimensional array parameters
+		public int FindTheCity(int n, int[][] edges, int distanceThreshold)
+#pragma warning restore S2368 // Public methods should not have multidimensional array parameters
+		{
+			int[,] nodeDistance = new int[n, n];
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					if (i == j)
+					{
+						nodeDistance[i, j] = 0;
+					}
+					else
+					{
+						nodeDistance[i, j] = (int)Math.Pow(10, 5);
+					}
+				}
+			}
+
+			foreach (var edge in edges)
+			{
+				int from = edge[0];
+				int to = edge[1];
+				int weight = edge[2];
+				nodeDistance[from, to] = weight;
+				nodeDistance[to, from] = weight;
+			}
+
+			for (int k = 0; k < n; k++)
+			{
+				for (int i = 0; i < n; i++)
+				{
+					for (int j = 0; j < n; j++)
+					{
+						if (nodeDistance[i, j] > nodeDistance[i, k] + nodeDistance[k, j])
+						{
+							nodeDistance[i, j] = nodeDistance[i, k] + nodeDistance[k, j];
+						}
+					}
+				}
+			}
+
+			int minNeighborCount = int.MaxValue;
+			int bestCity = -1;
+
+			for (int i = 0; i < n; i++)
+			{
+				int neighborCount = 0;
+				for (int j = 0; j < n; j++)
+				{
+					if (i != j &&
+						nodeDistance[i, j] <= distanceThreshold)
+					{
+						neighborCount++;
+					}
+				}
+
+				if (neighborCount <= minNeighborCount)
+				{
+					minNeighborCount = neighborCount;
+					bestCity = i;
+				}
+			}
+
+			return bestCity;
+		}
+	}
+}
---
+++ DailyChallenge/Program.cs
@@ -0,0 +1,7 @@
+﻿using DailyChallenge;
+
+var solution = new Solution01();
+
+var outptut = solution.FindTheCity(4, [[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]], 4);
+
+Console.WriteLine(outptut);
