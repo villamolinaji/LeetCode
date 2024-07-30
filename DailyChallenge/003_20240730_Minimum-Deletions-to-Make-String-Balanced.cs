36282e9c9abf7393e8b162e441dac37638648330
Jose Villa <Jose.Villa@version1.com>
2024-07-30 08:44:43 +0200

    Daily 20240730

---
+++ DailyChallenge/003_20240730_Minimum-Deletions-to-Make-String-Balanced.cs
@@ -0,0 +1,43 @@
+﻿namespace DailyChallenge
+{
+	public class Solution003
+	{
+		public int MinimumDeletions(string s)
+		{
+			if (s.Length < 2)
+			{
+				return 0;
+			}
+
+			int aCounter = 0;
+			int bCounter = 0;
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (s[i] == 'a')
+				{
+					bCounter++;
+				}
+			}
+
+			int returnValue = bCounter;
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (s[i] == 'a')
+				{
+					bCounter--;
+				}
+				else
+				{
+					aCounter++;
+				}
+
+				returnValue = Math.Min(aCounter + bCounter, returnValue);
+			}
+
+
+			return returnValue;
+		}
+	}
+}
--- DailyChallenge/Program.cs
+++ DailyChallenge/Program.cs
@@ -1,10 +1,13 @@
 ﻿using DailyChallenge;
 
-var solution = new Solution002();
+var solution = new Solution003();
 
 #pragma warning disable S125 // Sections of code should not be commented out
+
 //var outptut = solution.FindTheCity(4, [[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]], 4);
-var outptut = solution.NumTeams([2, 5, 3, 4, 1]);
+//var outptut = solution.NumTeams([2, 5, 3, 4, 1]);
+var outptut = solution.MinimumDeletions("baababbaabbaaabaabbabbbabaaaaaabaabababaaababbb");
+
 #pragma warning restore S125 // Sections of code should not be commented out
 
 Console.WriteLine(outptut);
