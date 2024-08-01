21efb344260855d50bb7cf05c966ea0efd8f8bee
Jose Villa <Jose.Villa@version1.com>
2024-08-01 12:14:49 +0200

    Daily 20240801

---
+++ DailyChallenge/005_20240801_Number-of-Senior-Citizens.cs
@@ -0,0 +1,12 @@
+﻿namespace DailyChallenge
+{
+	public class Solution005
+	{
+		public int CountSeniors(string[] details)
+		{
+			var ages = details.Select(details => int.Parse(details.Substring(11, 2))).ToList();
+
+			return ages.Count(age => age > 60);
+		}
+	}
+}
