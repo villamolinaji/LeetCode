5d75cc0c165c74c38cc5e754520a51d36e47069b
Jose Villa <Jose.Villa@version1.com>
2024-08-05 09:24:31 +0200

    Dialy 202040805

---
+++ DailyChallenge/007_20240805_Kth-Distinct-String-in-an-Array.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyChallenge
+{
+	public class Solution007
+	{
+		public string KthDistinct(string[] arr, int k)
+		{
+			var distincts = arr.GroupBy(x => x)
+				.Where(x => x.Count() == 1)
+				.Select(x => x.Key)
+				.ToList();
+
+			if (distincts.Count < k)
+			{
+				return string.Empty;
+			}
+
+			return distincts.Take(k).Last();
+		}
+	}
+}
--- DailyChallenge/Program.cs
+++ DailyChallenge/Program.cs
@@ -1,6 +1,6 @@
 ﻿using DailyChallenge;
 
-var solution = new Solution006();
+var solution = new Solution007();
 
 #pragma warning disable S125 // Sections of code should not be commented out
 
@@ -9,7 +9,8 @@ var solution = new Solution006();
 //var outptut = solution.MinimumDeletions("baababbaabbaaabaabbabbbabaaaaaabaabababaaababbb");
 //var outptut = solution.MinHeightShelves([[1, 1], [2, 3], [2, 3], [1, 1], [1, 1], [1, 1], [1, 2]], 4);
 //var outptut = solution.CountSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"]);
-var outptut = solution.MinSwaps([0, 1, 0, 1, 1, 0, 0]);
+//var outptut = solution.MinSwaps([0, 1, 0, 1, 1, 0, 0]);
+var outptut = solution.KthDistinct(["d", "b", "c", "b", "c", "a"], 2);
 
 #pragma warning restore S125 // Sections of code should not be commented out
 
