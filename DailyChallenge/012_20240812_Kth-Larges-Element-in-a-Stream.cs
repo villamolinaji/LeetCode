2cc11e829f4bbb1a59628fa89e789d9b0ddb0624
Jose Villa <Jose.Villa@version1.com>
2024-08-12 09:26:38 +0200

    Daily 20240812

---
+++ DailyChallenge/012_20240812_Kth-Larges-Element-in-a-Stream.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyChallenge
+{
+	public class KthLargest
+	{
+		public List<int> MyList { get; set; }
+		public int K { get; set; }
+
+		public KthLargest(int k, int[] nums)
+		{
+			K = k;
+			MyList = new List<int>(nums);
+			MyList.Sort();
+		}
+
+		public int Add(int val)
+		{
+			MyList.Add(val);
+			MyList.Sort();
+
+			return MyList[MyList.Count - K];
+		}
+	}
+}
