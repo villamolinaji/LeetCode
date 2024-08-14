29df49856b7bafed60227ba8b32e525b8056a8f7
Jose Villa <Jose.Villa@version1.com>
2024-08-14 10:08:29 +0200

    Daily 20240814

---
+++ DailyChallenge/014_20240814_Find-K-th-Smallest-Pair-Distance.cs
@@ -0,0 +1,42 @@
+﻿namespace DailyChallenge
+{
+	public class Solution014
+	{
+		public int SmallestDistancePair(int[] nums, int k)
+		{
+			Array.Sort(nums);
+
+			var numsLength = nums.Length;
+			var left = 0;
+			var right = nums[numsLength - 1] - nums[0];
+
+			while (left < right)
+			{
+				var mid = left + (right - left) / 2;
+				var count = 0;
+				var j = 0;
+
+				for (int i = 0; i < numsLength; i++)
+				{
+					while (j < numsLength &&
+						nums[j] - nums[i] <= mid)
+					{
+						j++;
+					}
+					count += j - i - 1;
+				}
+
+				if (count >= k)
+				{
+					right = mid;
+				}
+				else
+				{
+					left = mid + 1;
+				}
+			}
+
+			return left;
+		}
+	}
+}
--- DailyChallenge/Program.cs
+++ DailyChallenge/Program.cs
@@ -1,6 +1,6 @@
 ﻿using DailyChallenge;
 
-var solution = new Solution013();
+var solution = new Solution014();
 
 #pragma warning disable S125 // Sections of code should not be commented out
 
@@ -15,7 +15,8 @@ var solution = new Solution013();
 //var outptut = solution.NumberToWords(1234567);
 //var outptut = solution.SpiralMatrixIII(1, 4, 0, 0);
 //var outptut = solution.NumMagicSquaresInside([[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]]);
-var outptut = solution.CombinationSum2([10, 1, 2, 7, 6, 1, 5], 8);
+//var outptut = solution.CombinationSum2([10, 1, 2, 7, 6, 1, 5], 8);
+var outptut = solution.SmallestDistancePair([1, 3, 1], 1);
 
 #pragma warning restore S125 // Sections of code should not be commented out
 
