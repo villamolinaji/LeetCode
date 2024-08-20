a85092433e2a12a9e1af91b229c0d6c496537b72
Jose Villa <Jose.Villa@version1.com>
2024-08-20 07:00:28 +0200

    Daily solve 20240819 and 20240820

---
+++ DailyChallenge/016_20240819_2-Keys-Keyboard.cs
@@ -0,0 +1,24 @@
+﻿namespace DailyChallenge
+{
+	public class Solution016
+	{
+		public int MinSteps(int n)
+		{
+			var result = 0;
+			var divisor = 2;
+
+			while (n > 1)
+			{
+				while (n % divisor == 0)
+				{
+					result += divisor;
+					n /= divisor;
+				}
+
+				divisor++;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyChallenge/017_20240820_Stone-Game-II.cs
@@ -0,0 +1,51 @@
+﻿namespace DailyChallenge
+{
+	public class Solution017
+	{
+		private int[,]? MemoStones { get; set; }
+		private int[]? PrefixSum { get; set; }
+
+		public int StoneGameII(int[] piles)
+		{
+			int pilesLength = piles.Length;
+			MemoStones = new int[pilesLength, pilesLength + 1];
+			PrefixSum = new int[pilesLength + 1];
+
+			for (int i = pilesLength - 1; i >= 0; --i)
+			{
+				PrefixSum[i] = PrefixSum[i + 1] + piles[i];
+			}
+
+			return DP(0, 1);
+		}
+
+		private int DP(int i, int m)
+		{
+			int maxStones = 0;
+
+			if (MemoStones != null && PrefixSum != null)
+			{
+				int memoStonesLength = MemoStones.GetLength(0);
+
+				if (i == memoStonesLength)
+				{
+					return 0;
+				}
+
+				if (MemoStones[i, m] != 0)
+				{
+					return MemoStones[i, m];
+				}
+
+				for (int x = 1; x <= 2 * m && i + x <= memoStonesLength; ++x)
+				{
+					maxStones = Math.Max(maxStones, PrefixSum[i] - DP(i + x, Math.Max(m, x)));
+				}
+
+				MemoStones[i, m] = maxStones;
+			}
+
+			return maxStones;
+		}
+	}
+}
