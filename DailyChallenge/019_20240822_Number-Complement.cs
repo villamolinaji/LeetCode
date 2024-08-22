4fe52d14641a047d01b874a8f160214d60b06a85
Jose Villa <Jose.Villa@version1.com>
2024-08-22 08:23:07 +0200

    Daily 20240822

---
+++ DailyChallenge/019_20240822_Number-Complement.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace DailyChallenge
+{
+	public class Solution019
+	{
+		public int FindComplement(int num)
+		{
+			var numBinary = Convert.ToString(num, 2);
+			var sb = new StringBuilder();
+
+			foreach (var c in numBinary)
+			{
+				sb.Append(c == '0' ? '1' : '0');
+			}
+
+			return Convert.ToInt32(sb.ToString(), 2);
+		}
+	}
+}
