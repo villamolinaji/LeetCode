fd92fc471db5154ed25c06638a7bf8011c35ec39
Jose Villa <Jose.Villa@version1.com>
2024-08-24 17:12:30 +0200

    Daily 20240824

---
+++ DailyChallenge/021_20240824_Find-the-Close-Palindrome.cs
@@ -0,0 +1,56 @@
+﻿namespace DailyChallenge
+{
+	public class Solution021
+	{
+		public string NearestPalindromic(string n)
+		{
+			long longN = long.Parse(n);
+			var nLength = n.Length;
+
+			if (longN == 11)
+			{
+				return "9";
+			}
+
+			if (longN <= 10)
+			{
+				return (longN - 1).ToString();
+			}
+
+			if (longN == Math.Pow(10, nLength - 1) + 1)
+			{
+				return (longN - 2).ToString();
+			}
+
+			var candidates = new List<long>();
+			candidates.Add((long)Math.Pow(10, nLength) + 1);
+			candidates.Add((long)Math.Pow(10, nLength - 1) - 1);
+
+			long prefix = long.Parse(n.Substring(0, (nLength + 1) / 2));
+
+			foreach (long i in new long[] { prefix - 1, prefix, prefix + 1 })
+			{
+				string pal = i.ToString() + new string(i.ToString().Reverse().Skip(nLength % 2).ToArray());
+				candidates.Add(long.Parse(pal));
+			}
+
+			long closestPalindrome = -1;
+			foreach (long candidate in candidates)
+			{
+				if (candidate == longN)
+				{
+					continue;
+				}
+
+				if (closestPalindrome == -1 ||
+					Math.Abs(candidate - longN) < Math.Abs(closestPalindrome - longN) ||
+					(Math.Abs(candidate - longN) == Math.Abs(closestPalindrome - longN) && candidate < closestPalindrome))
+				{
+					closestPalindrome = candidate;
+				}
+			}
+
+			return closestPalindrome.ToString();
+		}
+	}
+}
--- DailyChallenge/Program.cs
+++ DailyChallenge/Program.cs
@@ -1,6 +1,6 @@
 ﻿using DailyChallenge;
 
-var solution = new Solution015();
+var solution = new Solution021();
 
 #pragma warning disable S125 // Sections of code should not be commented out
 
@@ -17,7 +17,8 @@ var solution = new Solution015();
 //var outptut = solution.NumMagicSquaresInside([[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]]);
 //var outptut = solution.CombinationSum2([10, 1, 2, 7, 6, 1, 5], 8);
 //var outptut = solution.SmallestDistancePair([1, 3, 1], 1);
-var outptut = solution.MaxDistance([[1, 4], [0, 5]]);
+//var outptut = solution.MaxDistance([[1, 4], [0, 5]]);
+var outptut = solution.NearestPalindromic("10001");
 
 #pragma warning restore S125 // Sections of code should not be commented out
 
