fe1318fc66b16221fb5463dc6dd2ec8b542bafb8
Jose Villa <Jose.Villa@version1.com>
2024-08-25 11:26:56 +0200

    Daily 20240825

---
+++ DailyChallenge/022_20240825_Binary-Tree-Postorder-Traversal.cs
@@ -0,0 +1,57 @@
+﻿namespace DailyChallenge
+{
+	public class TreeNode
+	{
+		public int val { get; set; }
+		public TreeNode? left { get; set; }
+		public TreeNode? right { get; set; }
+		public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public class Solution022
+	{
+		public IList<int> PostorderTraversal(TreeNode root)
+		{
+			var result = new List<int>();
+			if (root == null)
+			{
+				return result;
+			}
+
+			var stack = new Stack<TreeNode>();
+			TreeNode? lastVisited = null;
+			var current = root;
+
+			while (current != null ||
+				stack.Count > 0)
+			{
+				if (current != null)
+				{
+					stack.Push(current);
+					current = current.left;
+				}
+				else
+				{
+					var peekNode = stack.Peek();
+					if (peekNode.right != null &&
+						lastVisited != peekNode.right)
+					{
+						current = peekNode.right;
+					}
+					else
+					{
+						result.Add(peekNode.val);
+						lastVisited = stack.Pop();
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyChallenge/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Bug", "S1244:Floating point numbers should not be tested for equality", Justification = "<Pending>", Scope = "member", Target = "~M:DailyChallenge.Solution021.NearestPalindromic(System.String)~System.String")]
