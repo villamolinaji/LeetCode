97fab89a066c7775bd7b76bcb7e00500a2210703
Jose Villa <Jose.Villa@version1.com>
2024-10-07 09:47:16 +0200

    DynamicProgramming solve 17 to 19

---
+++ DynamicProgramming/17_Minimum-ASCII-Delete-Sum-for-Two-Strings.cs
@@ -0,0 +1,40 @@
+﻿namespace DynamicProgramming
+{
+	public class Solution17
+	{
+		public int MinimumDeleteSum(string s1, string s2)
+		{
+			int s1Length = s1.Length;
+			int s2Length = s2.Length;
+
+			int[,] dp = new int[s1Length + 1, s2Length + 1];
+
+			for (int i = 1; i <= s1Length; i++)
+			{
+				dp[i, 0] = dp[i - 1, 0] + (int)s1[i - 1];
+			}
+
+			for (int j = 1; j <= s2Length; j++)
+			{
+				dp[0, j] = dp[0, j - 1] + (int)s2[j - 1];
+			}
+
+			for (int i = 1; i <= s1Length; i++)
+			{
+				for (int j = 1; j <= s2Length; j++)
+				{
+					if (s1[i - 1] == s2[j - 1])
+					{
+						dp[i, j] = dp[i - 1, j - 1];
+					}
+					else
+					{
+						dp[i, j] = Math.Min(dp[i - 1, j] + (int)s1[i - 1], dp[i, j - 1] + (int)s2[j - 1]);
+					}
+				}
+			}
+
+			return dp[s1Length, s2Length];
+		}
+	}
+}
---
+++ DynamicProgramming/18_Distinct-Subsequences.cs
@@ -0,0 +1,40 @@
+﻿namespace DynamicProgramming
+{
+	public class Solution18
+	{
+		public int NumDistinct(string s, string t)
+		{
+			int sLength = s.Length;
+			int tLength = t.Length;
+
+			int[,] dp = new int[sLength + 1, tLength + 1];
+
+			for (int i = 0; i <= sLength; i++)
+			{
+				dp[i, 0] = 1;
+			}
+
+			for (int j = 1; j <= tLength; j++)
+			{
+				dp[0, j] = 0;
+			}
+
+			for (int i = 1; i <= sLength; i++)
+			{
+				for (int j = 1; j <= tLength; j++)
+				{
+					if (s[i - 1] == t[j - 1])
+					{
+						dp[i, j] = dp[i - 1, j] + dp[i - 1, j - 1];
+					}
+					else
+					{
+						dp[i, j] = dp[i - 1, j];
+					}
+				}
+			}
+
+			return dp[sLength, tLength];
+		}
+	}
+}
