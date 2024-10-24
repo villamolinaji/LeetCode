578f7768b30fd703e6073725c3e318e338997829
Jose Villa <Jose.Villa@version1.com>
2024-10-24 12:56:39 +0200

    BinarySearch solve 22 to 25

---
+++ BinarySearch/22_Single-Element-in-a-Sorted-Array.cs
@@ -0,0 +1,32 @@
+﻿namespace BinarySearch
+{
+	public class Solution22
+	{
+		public int SingleNonDuplicate(int[] nums)
+		{
+			int left = 0;
+			int right = nums.Length - 1;
+
+			while (left < right)
+			{
+				int mid = left + (right - left) / 2;
+
+				if (mid % 2 == 1)
+				{
+					mid--;
+				}
+
+				if (nums[mid] == nums[mid + 1])
+				{
+					left = mid + 2;
+				}
+				else
+				{
+					right = mid;
+				}
+			}
+
+			return nums[left];
+		}
+	}
+}
---
+++ BinarySearch/24_Find-K-Closest-Elements.cs
@@ -0,0 +1,30 @@
+﻿namespace BinarySearch
+{
+	public class Solution24
+	{
+		public IList<int> FindClosestElements(int[] arr, int k, int x)
+		{
+			int left = 0;
+			int right = arr.Length - k;
+
+			while (left < right)
+			{
+				int mid = left + (right - left) / 2;
+
+				if (x - arr[mid] > arr[mid + k] - x)
+				{
+					left = mid + 1;
+				}
+				else
+				{
+					right = mid;
+				}
+			}
+
+			return arr
+				.Skip(left)
+				.Take(k)
+				.ToList();
+		}
+	}
+}
