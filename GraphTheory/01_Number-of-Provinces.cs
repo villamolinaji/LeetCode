60f90306b51db2fff949b8e9ba6bda208159f96b
Jose Villa <Jose.Villa@version1.com>
2024-10-25 10:23:22 +0200

    Add GraphTheory project

---
+++ GraphTheory/01_Number-of-Provinces.cs
@@ -0,0 +1,36 @@
+﻿namespace GraphTheory
+{
+	public class Solution01
+	{
+		public int FindCircleNum(int[][] isConnected)
+		{
+			int sConnectedLength = isConnected.Length;
+			bool[] visited = new bool[sConnectedLength];
+			int provinceCount = 0;
+
+			for (int i = 0; i < sConnectedLength; i++)
+			{
+				if (!visited[i])
+				{
+					DFS(isConnected, visited, i);
+					provinceCount++;
+				}
+			}
+
+			return provinceCount;
+		}
+
+		private void DFS(int[][] isConnected, bool[] visited, int i)
+		{
+			visited[i] = true;
+
+			for (int j = 0; j < isConnected.Length; j++)
+			{
+				if (isConnected[i][j] == 1 && !visited[j])
+				{
+					DFS(isConnected, visited, j);
+				}
+			}
+		}
+	}
+}
---
+++ GraphTheory/Program.cs
@@ -0,0 +1,7 @@
+﻿using GraphTheory;
+
+var solution = new Solution01();
+
+var outptut = solution.FindCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]]);
+
+Console.WriteLine(outptut);
